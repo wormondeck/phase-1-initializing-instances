313d14abbf7ff2fe8e6b60c93eb682c1b195e5f6
Ronney Arias <ronneyarias@gmail.com>
2023-11-28 10:01:50 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    #entree;
+    constructor(salad, soup, entree) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+       
+    }
+}
